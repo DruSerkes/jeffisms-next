4cc012b6fc595367c2de0ca5a9a5f21f073cf6ef
Dru Serkes <druserkes@gmail.com>
2023-01-20 18:19:05 -0500

    Fix layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,27 @@
 import { Nunito } from '@next/font/google'
 import styles from '@/styles/Layout.module.css'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
 export default function Layout({ children }: React.PropsWithChildren) {
+  const [image, setImage] = useState({ height: 800, width: 300 })
+  useEffect(() => {
+    if (!window) return;
+    const width = window.innerWidth / 4;
+    const height = window.innerHeight;
+    setImage({ width, height })
+  }, [window])
+
   return (
     <>
       <main className={styles.main}>
         <div className={styles.left}>
           <Image
             src='/../public/jeffrey-d-serkes.jpeg'
-            height={window.innerHeight}
-            width={window.innerWidth / 4}
+            height={image.height}
+            width={image.width}
             alt="The Man, The Myth, The Legend"
           />
         </div>
