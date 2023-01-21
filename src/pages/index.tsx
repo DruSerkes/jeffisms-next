e1bcc2cb881b83f3cd4e65f18f4c426b09f2415f
Dru Serkes <druserkes@gmail.com>
2023-01-20 19:51:11 -0500

    parse the CSV, update styles, show em all for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,61 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Layout from '@/components/layout'
+import { parse } from 'csv-parse'
+import fs from 'fs'
+import { Jeffism } from '@/types/types'
 
 
 
-export default function Home() {
+export async function getStaticProps() {
+  // let jeffisms: Jeffism[] = []
+  const data = fs.readFileSync('src/jeffisms.csv', 'utf-8')
+  const jeffisms = parse(data, { trim: true, columns: false }, (err, rows: string[][]) => {
+    if (err) throw err;
+
+    return rows.slice(1).map(row => {
+      const [saying, definition, definition2, source] = row;
+      return {
+        saying,
+        definition,
+        definition2,
+        source
+      }
+    })
+  })
+
+  return {
+    props: {
+      jeffisms
+    }
+  }
+}
+
+interface Props {
+  jeffisms: Jeffism[];
+}
+
+export default function Home({ jeffisms }: Props) {
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>Jeffisms</title>
-          <meta name="description" content="Wisdom, or whatever, by Jeff!" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className={styles.main}>
-          <p>Jeffism Here</p>
-        </main>
-      </Layout>
-    </>
+    <Layout>
+      <Head>
+        <title>Jeffisms</title>
+        <meta name="description" content="Wisdom, or whatever, by Jeff!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          {jeffisms.map(jeffism => (
+            <div className={styles.card}>
+              <h2>"{jeffism.saying}"</h2>
+              <p> - {jeffism.source}</p>
+              <span><b>Definition:</b> {jeffism.definition}</span>
+              {jeffism.definition2 && <span><b>Alt Definition:</b> {jeffism.definition2}</span>}
+            </div>
+          ))}
+        </div>
+      </main>
+    </Layout>
   )
 }
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export interface Jeffism {
+  saying: string;
+  definition: string;
+  definition2: string;
+  source: string;
+}
